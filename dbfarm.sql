5cfcd89708d618ddbcd6d0f0656bebb3a8992546
Engharun <136518203+Engharun@users.noreply.github.com>
2024-03-22 17:50:42 +0300

    Add files via upload

--- dbfarm.sql
+++ dbfarm.sql
@@ -1,5 +1,14 @@
 create database DBfarm
 
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY IDENTITY,
+    Username VARCHAR(50) UNIQUE NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    
+);
+
+   
 CREATE TABLE Crops(
     Crop_No INT PRIMARY KEY,
     Crop_Name VARCHAR(255) NOT NULL,
@@ -39,6 +48,15 @@ CREATE TABLE Post_Advertisement (
    
 );
 
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100),
+    Phone VARCHAR(20),
+    Address VARCHAR(255)
+);
+
 CREATE TABLE Sell_Crop_Details (
     sell_id INT PRIMARY KEY,
     Crop_Name VARCHAR(255) NOT NULL,
@@ -52,6 +70,77 @@ CREATE TABLE Farmer_Complaints (
     FOREIGN KEY (Farmer_id) REFERENCES Farmers_Details(Farmer_id)
 );
 
+CREATE TABLE WeatherData (
+    DataID INT PRIMARY KEY,
+    Date DATE,
+    Temperature DECIMAL(5, 2),
+    Rainfall DECIMAL(8, 2),
+    Humidity DECIMAL(5, 2),
+    WindSpeed DECIMAL(6, 2)
+);
 
+CREATE TABLE PestsAndDiseases (
+    IssueID INT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Description TEXT,
+    Lifecycle TEXT,
+    ControlMethods TEXT,
+    TreatmentPlan TEXT
+);
+
+CREATE TABLE FinancialRecords (
+    TransactionID INT PRIMARY KEY,
+    Date DATE,
+    TransactionType VARCHAR(100) NOT NULL,
+    Amount DECIMAL(12, 2),
+    Description TEXT
+);
+CREATE TABLE RegulatoryCompliance (
+    ComplianceID INT PRIMARY KEY,
+    Requirement VARCHAR(255) NOT NULL,
+    Deadline DATE,
+    Status VARCHAR(50),
+    Description TEXT
+);
+
+
+CREATE TABLE TrainingPrograms (
+    ProgramID INT PRIMARY KEY,
+    ProgramName VARCHAR(100) NOT NULL,
+    Description TEXT,
+    StartDate DATE,
+    EndDate DATE,
+    Location VARCHAR(100)
+);
+
+
+
+
+
+
+CREATE TABLE FarmingEquipment (
+    EquipmentID INT PRIMARY KEY,
+    EquipmentName VARCHAR(100) NOT NULL,
+    Description TEXT,
+    Manufacturer VARCHAR(100),
+    Model VARCHAR(50),
+    YearOfManufacture INT,
+    PurchaseDate DATE,
+    Location VARCHAR(100),
+    Availability BIT
+);
+
+CREATE TABLE HarvestingTools (
+    ToolID INT PRIMARY KEY,
+    ToolName VARCHAR(100) NOT NULL,
+    Description TEXT,
+    Manufacturer VARCHAR(100),
+    Model VARCHAR(50),
+    Location VARCHAR(100),
+    Availability BIT
+);
+
+
+select * from FarmingEquipment
 
 
